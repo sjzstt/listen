873f4f02e8683cd09c221a501e5aa2d23d147c65
Bob-Name <1474218225@qq.com>
2018-10-27 10:34:14 +0800

    master 主框架

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+*{
+  margin:0;
+  padding:0;
+}
+ul{
+  list-style:none;
+}
+a,img{
+  display:block;
+  text-decoration: none;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
